0ff90343edf25d3d36eb1c8b34260adce0be3c06
romanananakin <roman.anankin@betconstruct.com>
2019-06-28 11:45:19 +0300

    string size def added

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -3,13 +3,34 @@ package com;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        List<String> strings = Files.readAllLines(Paths.get("/home/roman/Documents/concatenatedwords/src/main/resources/words.txt"));
-        List<String> allConcatenatedWordsInADict = new ConcatenetedWords()
-                .findAllConcatenatedWordsInADict(strings.toArray(new String[0]));
-        System.out.println(allConcatenatedWordsInADict.size());
+        List<String> strings = Files
+                .readAllLines(Paths.get("src/main/resources/words.txt"));
+        List<String> allConcatenatedWordsInADict = findAllConcatenatedWordsInADict(strings.toArray(new String[0]));
+
+        final int size = allConcatenatedWordsInADict.size();
+
+        allConcatenatedWordsInADict.sort((Comparator.comparingInt(String::length)));
+        String longest = allConcatenatedWordsInADict.get(allConcatenatedWordsInADict.size() - 1);
+        String secondLongest = allConcatenatedWordsInADict.get(allConcatenatedWordsInADict.size() - 2);
+        System.out.println("The number of concatenated words is " + size +
+                " and the longest word is " +
+                longest + "and " + secondLongest + " goes after");
+    }
+
+    private static List<String> findAllConcatenatedWordsInADict(String[] words) {
+        List<String> res = new ArrayList<>();
+        if (words == null || words.length < 2) return res;
+        Trie root = new Trie();
+        for (String word : words) {
+            if (!word.isEmpty()) root.add(word, 0);
+        }
+        for (String word : words) {
+            if (root.match(word, 0)) res.add(word);
+        }
+        return res;
     }
 }
